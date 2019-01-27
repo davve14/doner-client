ad6a37eb28926e3b1f7c752213a85448e685ab21
David Aspegren <5038369+davve14@users.noreply.github.com>
2019-01-27 18:05:22 +0100

    changed flexi on about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
-    <v-container style="width:960px;">
+    <v-container style="max-width: 960px;">
       
         <h1 class="headline">About The Berlin Döner Database</h1>
         <p>
@@ -9,10 +9,10 @@
           description of the dishes easier. You can read all about it right here!
         </p>
 
-        <h2 class="title">About the Veal Döner</h2>
+        <h2 class="title">About Veal Döner</h2>
         <v-layout row wrap>
           <v-flex xs12 md6>
-        <p>
+            
           The Veal döner pivots around the veal meat, which, in turn, pivots around a skewer while being 
           cooked rotisserie style. The meat is later carved off in thin shavings and put inside a sandwich 
           together with vegetables (usually some combination of tomatoes, cucumber, onions, lettuce and 
@@ -28,7 +28,7 @@
           tradition. Leading examples are Doyum Döner, Pamfilya and Tadim. There are plenty of other 
           choices, so make sure to do your reserach properly before making a spontaneous street dinner 
           reservation. Luckily you have come to the right place, as you can do this research right here!
-        </p>
+        
 </v-flex>
 <v-flex xs12 md6>
 <figure class="figure_wrapp">
@@ -58,13 +58,15 @@ particurly when waking up parched after a heavy night of drinking. The fatty mea
 salt your body craves! But please do your research beforehand.</p>
 
 <h2 class="title">About Chicken Döner</h2>
-<div class="img_container">
-
+<v-layout row wrap>
+  <v-flex xs12 md6>
 <figure class="figure_wrapp">
         <v-img :src="require('@/assets/ruya_spett.png')" width="400" aspect-ratio="1" alt="Tadim Döner">
         </v-img>
         <figcaption>Rüya Gemüse Kebab - Chicken Döner in Berlin, Schöneberg</figcaption>
       </figure>
+      </v-flex>
+      <v-flex xs12 md6>
 The Chicken Döner consists of chicken filets on a skewer cooked rotisserie style. Since chicken 
 lacks the fat levels the Veal option have, Chicken Döner makers often spice up the skewer with 
 vegetables and a more aggressive seasoning. The lesser fat also maintain the illusion of the chicken 
@@ -76,48 +78,34 @@ spearheaded by the likes of Mustafa and Rüyam. This trend flows from a line of 
 seems to encourage a certain kind of inventiveness and has enriched the Döner-society with a 
 couple of new influences like adding spice and oil to the meat, adding roasted veggies, sprinkles of 
 turkish feta cheese and some sour lemon.
-</div>
-
+</v-flex>
+</v-layout>
+<br>
 <h2 class="title">About Falafel</h2>
 <p>
 Falafel is 100% vegan and consists of a deep fried ball of chick peas, fava beans or both. It's a 
-
 traditional middle-eastern food which traces its origin back to Egypt from where it migrated north 
-
 to the Levant and later spread its tastiness across the world.</p>
 <p>
 The beans are prepared and later mixed with other components like, for example, scallions, garlic 
-
 and parsley. A variety of spices like celantral or cummin are added. The balls are then added to a 
-
 sandwich together with an assortment of other vegetables and some usually very delicous sauces.
-
 Dear potential reader, we must issue a warning about falafel. A lot imbisses don't really prepare 
-
 falafels properly but simply procure them from Lidl or other less authentic sources. These ones are 
-
 with very few exceptions really goddamn terrible. What you desire – and what we want to help you 
-
 with – is to find the one which prepare the Falafel themselves. These ones are superior in every 
-
 respect.</p>
 <p>
 Falafel isn't really Döner since that is, as previously discussed, meat cooked slowly on a skewer and
-
 Falafel isn't even meat. Falafel is, however, a lovely tasty dish supplied by several imbisses and that
-
 is what matters to us. Try, for example, Zweistrom.</p>
-
 <h2 class="title">About veggies</h2>
-
+<p>
 Veggie Döners can take about any shape. I mean, in part the Falafel is a vegetarian type of 
-
 vegetarian Döner. But that type of sandwich has been honored with its own category. So here we'll 
-
 look into other, often innovative, versions of the Döner sandwich. Like our friends down at Vöner. 
-
 Brilliant.
-
+</p>
 </v-container>
 
   </div>
