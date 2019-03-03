7660a6a626e28302f700b887325622884baada4c
David Aspegren <5038369+davve14@users.noreply.github.com>
2019-03-03 11:32:28 +0100

    fix list bug

--- src/App.vue
+++ src/App.vue
@@ -25,8 +25,7 @@
               </v-btn>
             </v-list-tile-content>
           </v-list-tile>
-        </v-list>
-        <v-list-tile>
+          <v-list-tile>
             <v-list-tile-content>
               <v-btn right flat href="/about">
                 <span class="font-weight-light">About</span>
@@ -40,36 +39,32 @@
               </v-btn>
             </v-list-tile-content>
           </v-list-tile>
+        </v-list>
       </v-menu>
       <v-toolbar-items>
         <v-btn class="hidden-sm-and-down" right flat href="/home">
-        <span class="headline">Berlin</span>
-        <span class="headline font-weight-light">&nbsp;Döner Database</span>
+          <span class="headline">Berlin</span>
+          <span class="headline font-weight-light">&nbsp;Döner Database</span>
         </v-btn>
         <v-btn class="hidden-md-and-up" right flat href="/home">
-        <span class="headline">BERLIN</span>
-        <span class="headline font-weight-light">&nbsp;Döner DB</span>
+          <span class="headline">BERLIN</span>
+          <span class="headline font-weight-light">&nbsp;Döner DB</span>
         </v-btn>
       </v-toolbar-items>
-    <v-toolbar-items class="ml-5 hidden-sm-and-down">
-      <v-btn class="font-weight-light" to="/" flat>Explore</v-btn>
-      <v-btn class="font-weight-light" to="/map" flat>Map</v-btn>
-      <v-btn class="font-weight-light" to="/about" flat>About</v-btn>
-      <v-text-field append-icon="search" clearable @keyup.enter="redirectToSearch()" v-model="globalFilterName" 
-        class="mt-1 ml-5 font-weight-light"
-        label="Imbiß search"
-      ></v-text-field>
-    </v-toolbar-items>
+      <v-toolbar-items class="ml-5 hidden-sm-and-down">
+        <v-btn class="font-weight-light" to="/" flat>Explore</v-btn>
+        <v-btn class="font-weight-light" to="/map" flat>Map</v-btn>
+        <v-btn class="font-weight-light" to="/about" flat>About</v-btn>
+        <v-text-field append-icon="search" clearable @keyup.enter="redirectToSearch()" v-model="globalFilterName" class="mt-1 ml-5 font-weight-light"
+          label="Imbiß search"></v-text-field>
+      </v-toolbar-items>
       <v-spacer></v-spacer>
-    <v-toolbar-items class="hidden-sm-and-down">
-      <v-btn
-        flat
-        to="/contact"
-      >
-      <v-icon left>email</v-icon><span class="hidden-sm-and-down font-weight-light">Contact us</span>
-      </v-btn>
-    </v-toolbar-items>
-    </v-toolbar> 
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn flat to="/contact">
+          <v-icon left>email</v-icon><span class="hidden-sm-and-down font-weight-light">Contact us</span>
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
     <v-content>
       <router-view :globalFilterName="globalFilterName"></router-view>
     </v-content>
